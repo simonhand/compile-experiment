6d3806024691de65674278770339beead90530e7
zhangle <1029838709@qq.com>
2021-05-29 10:04:27 +0800

    0.1

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,90 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-header class="header">
+        <div>安徽工业大学</div>
+        <div>编译原理实验</div>
+        <div>189074335张乐</div>
+      </el-header>
+    <el-container class="container">
+      
+      <el-aside class="aside">
+        <el-menu default-active="" :default-openeds="['1']">
+          <el-submenu index="1" :default-openeds="['0']">
+            <template slot="title"
+              ><i class="el-icon-setting"></i>选择实验</template
+            >
+            <el-menu-item index="1" @click="experiment1">实验一</el-menu-item>
+            <el-menu-item index="2">实验二</el-menu-item>
+            <el-menu-item index="3">实验三</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: "App",
+  components: {},
+  methods: {
+    experiment1: function () {
+      console.log("ahahh");
+    },
+  },
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.container {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  border: 1px solid #eee;
+}
+.header {
+  position: relative;
+  top: -10px;
+  display: flex;
+  line-height: 60px;
+  text-align: center;
+  justify-content: space-between;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #409eff;
+  box-shadow: 0 10px 0.8px rgba(0,0,0,0.1);
+}
+.header :nth-child(1) {
+  padding-left: 60px;
+  font-size: 28px;
+  color: #eee;
+}
+.header :nth-child(2) {
+  font-size: 28px;
+  color: #eee;
+}
+.header :nth-child(3) {
+  font-size: 18px;
+  color: #eee;
+  padding-right: 20px;
+}
+.aside {
+  width: 200px;
+  height: 650px;
+  background-color: rgb(238, 241, 246);
+}
+.main {
+  background-color: black;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,82 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="top">
+      <div class="title1">实验一：计算First集</div>
+      <div class="title2">
+        实验要求:输入：任何一个上下文无关文法输出：该上下文无关文法的一切非终结符的First集合
+      </div>
+    </div>
+    <div class="context">
+      <el-select v-model="select" slot="prepend" placeholder="请选择" class="select">
+          <el-option  v-for='(item,index) in flag' :label="item" :value="item" :key="index"></el-option>
+        </el-select>
+      <el-input
+        placeholder="文法"
+        v-model="data.wenfa"
+        show-word-limit
+        class="dataInput"
+      >
+        
+        <el-button slot="prepend" icon="el-icon-right"></el-button
+      ></el-input>
+      <el-button type="primary" @click="submit">提交</el-button>
+    </div>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      select:'',
+      data: {
+        name:'S',
+        wenfa:'aB'
+      },
+      info:{
+        wf:'',
+        wenfa:[]
+      },
+      flag:['S','G',"L","R"]
+    };
+  },
+  methods: {
+    submit:function(){
+      console.log(this.select);
+    }
+  },
+};
 </script>
+<style scoped>
+.top {
+  margin: 5px;
+  border: 1px solid #eee;
+}
+.title1 {
+  padding: 20px 20px;
+  font-family: "Helvetica Neue";
+  text-align: left;
+  color: #a1a1a1;
+}
+.title2 {
+  padding-left: 20px;
+  padding-bottom: 20px;
+  font-family: "Helvetica Neue";
+  text-align: left;
+  color: #a1a1a1;
+}
+.context {
+  display: flex;
+  margin-top: 20px;
+  padding-left: 20px;
+  width: 800px;
+}
+.dataInput {
+  width: 300px;
+}
+.select{
+  width: 100px;
+}
+</style>
